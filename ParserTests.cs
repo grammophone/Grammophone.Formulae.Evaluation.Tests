e96a9f86428e50f279768e4b1b6a86568c54b006
grammophone <emmanuel.liossis@gmail.com>
2024-07-31 21:52:27 -0400

    Add tests for FormulaParser.

---
+++ ParserTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Grammophone.Formulae.Evaluation.Tests
+{
+	[TestClass]
+	public class ParserTests
+	{
+		[TestMethod]
+		public void DiagnoseNoError()
+		{
+			var formulaEvaluatorFactory = new FormulaEvaluatorFactory<EmployeeContext>();
+
+			string expression = "2 + 2";
+
+			var parser = formulaEvaluatorFactory.GetFormulaParser();
+
+			var diagnostics = parser.Validate(expression);
+
+			Assert.IsFalse(diagnostics.Any(), "There should be no errors.");
+		}
+
+		[TestMethod]
+		public void DiagnoseMissingParenthesis()
+		{
+			var formulaEvaluatorFactory = new FormulaEvaluatorFactory<EmployeeContext>();
+
+			string expression = "(2 + 2";
+
+			var parser = formulaEvaluatorFactory.GetFormulaParser();
+
+			var diagnostics = parser.Validate(expression);
+
+			Assert.IsTrue(diagnostics.Any(), "There should be an error.");
+		}
+
+		[TestMethod]
+		public void DiagnosValidVariables()
+		{
+			var formulaEvaluatorFactory = new FormulaEvaluatorFactory<EmployeeContext>();
+
+			string expression = "42 + B";
+
+			var parser = formulaEvaluatorFactory.GetFormulaParser();
+
+			var diagnostics = parser.Validate(expression);
+
+			Assert.IsFalse(diagnostics.Any(), "There should be no errors.");
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(FormulaNameAccessException))]
+		public void FailForbiddenName()
+		{
+			string[] excludedNames = new[] { "B" }; 
+
+			var formulaEvaluatorFactory = new FormulaEvaluatorFactory<EmployeeContext>(excludedNames: excludedNames);
+
+			string expression = "42 + B";
+
+			var parser = formulaEvaluatorFactory.GetFormulaParser();
+
+			var diagnostics = parser.Validate(expression);
+
+			Assert.IsFalse(diagnostics.Any(), "There should be no errors.");
+		}
+	}
+}
